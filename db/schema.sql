86e4856aaaf43a1b1f7bedce3bee9876ce2b4cca
Jarret Yingling <44506154+JarretYingling@users.noreply.github.com>
2019-03-06 19:46:30 -0500

    general file and folder structure with seeds.sql

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS stock_picks_db;
+CREATE DATABASE stock_picks_db;
---
+++ test/mocha.test.js
@@ -0,0 +1,14 @@
+"use strict";
+const log = console.log;
+
+/*
+const model = require("./models/");
+
+const amount = 100;
+const stock = "fb";
+const user = "jarret";
+
+model.trx.buy(amount, stock, user);
+*/
+
+log("mocha test executed successfully from ./test/mocha.test.js");
